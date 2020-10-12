0acd3849a7d1190d332ad12b21c47bda3279d8d8
坪田健人 <lyla.0218@gmail.com>
2020-10-12 21:59:22 +0900

    userのcreate,update,showを実装

--- db/migrate/20201010212332_create_users.rb
+++ db/migrate/20201010212332_create_users.rb
@@ -1,20 +1,22 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :nick_name
-      t.string :last_name
-      t.string :first_name
-      t.integer :gender
-      t.string :email
-      t.string :password_digest
-      t.string :zip_code
-      t.string :prefecture
-      t.string :city
-      t.string :house_number
+      t.string :nick_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.integer :gender, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :zip_code, null: false
+      t.string :prefecture, null: false
+      t.string :city, null: false
+      t.string :house_number, null: false
       t.string :tel
-      t.string :building_name
+      t.string :building_name, null: false
 
       t.timestamps
     end
   end
 end
+
+
