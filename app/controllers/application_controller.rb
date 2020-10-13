cf6ece7c3c8a182415f028683bd5d5c68d4776fb
坪田健人 <lyla.0218@gmail.com>
2020-10-13 21:47:52 +0900

    ユーザーのログイン・ログアウト機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
   before_action :authenticate!
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :show, :update]
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
