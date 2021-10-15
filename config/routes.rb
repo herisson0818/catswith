7ef320fef6d30f2acc2bfc8ce6d9ff60ff8c3ff3
herisson0818 <lyla.0218@gmail.com>
2021-10-15 22:33:51 +0900

    api仕様を解除する

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
   get "areas/create"
   get "areas/show"
   get "areas/index"
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create, :show, :update]
-      resources :cats, only: [:create, :show, :update]
-      resources :cat_favorites, only: [:create, :destroy]
-      resources :feedings, only: [:create, :destroy]
-      resources :images, only: [:create, :destroy, :show]
-      resources :likes, only: [:create, :destroy]
-      resources :areas, only: [:create, :show, :index, :update]
-      post "/login", to: "sessions#create"
-      delete "/logout", to: "sessions#destroy"
-    end
-  end
+  #namespace :api do
+  #  namespace :v1 do
+  resources :users, only: [:create, :show, :update]
+  resources :cats, only: [:create, :show, :update]
+  resources :cat_favorites, only: [:create, :destroy]
+  resources :feedings, only: [:create, :destroy]
+  resources :images, only: [:create, :destroy, :show]
+  resources :likes, only: [:create, :destroy]
+  resources :areas, only: [:create, :show, :index, :update]
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  #    end
+  #  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
